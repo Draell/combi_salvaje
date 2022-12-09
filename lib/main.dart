d8b36de7ef72aa6cac1615e5a92ae2c2ae9e5e6a
Draell <vic-link@hotmail.com>
2022-12-08 20:33:26 -0600

    Segundo commit. Estructura general, navegación, login y registro.

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,55 @@
+import 'package:combi_salvaje/routes/home.dart';
+import 'package:combi_salvaje/routes/login.dart';
+import 'package:combi_salvaje/routes/footer_menu.dart';
+import 'package:combi_salvaje/routes/register.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main(){
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'CombiSalvaje';
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
-      ),
+    var myAppBar = AppBar(
+      backgroundColor: Colors.deepPurple,
+      foregroundColor: Colors.white,
+      title: Text("Combi-Salvaje"),
     );
+
+    return MaterialApp(
+        /*home: Scaffold(
+          appBar: myAppBar,
+          bottomNavigationBar: navb,
+        ),
+*/
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          primaryColor: Colors.purple[800],
+        ),
+        initialRoute: '/home',
+        routes: {
+          //'/footer_menu': (context) => FooterMenu(context),
+          '/home': (context) => const Home(),
+          '/register': (context) => const Register(),
+          '/login': (context) => const Login(),
+          //'/map': (context) => const MapScreen(),
+          //'/user': (context) => const UserScreen(),
+          //'/driver': (context) => const DriverScreen(),
+        },
+    );
+
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+/*class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Combi Salvaje',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Inicia Sesión',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Correo',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Contraseña',
-                ),
-              ),
-            ),
-
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Vamos',),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )
-            ),
-          ],
-        ));
+  Widget build(BuildContext context){
+    return const Scaffold();
   }
-}
+
+  }*/
+
